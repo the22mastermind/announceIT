e45c4bcd44b02b5c9312beb9a109b1da2dc63831
themastermind <bertrandmasabo22@gmail.com>
2020-01-23 19:08:25 +0200

    Add user sign in functionality

--- server/utils/messages.js
+++ server/utils/messages.js
@@ -1,8 +1,2 @@
-exports.successfulSignup = 'User created successfully';
-exports.userExists = 'User already registered. Please use a different email or reset your password';
-exports.passwordsNoMatch = 'Please make sure your passwords are matching';
-exports.welcomeMessage = 'Welcome to AnnouneIT!';
-exports.emptyEmail = 'email is not allowed to be empty';
-exports.invalidEmail = 'email must be a valid email';
-exports.emptyFirstName = 'firstname is required';
-exports.invalidFirstName = 'firstname length must be at least 3 characters long';
+exports.invalidCredentials = 'Invalid username or password. Please try again.';
+exports.successfulLogin = 'Hello, welcome!';
