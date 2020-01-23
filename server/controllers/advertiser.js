c24d2d1c28af49b5d464547d13d45012dd04a5e8
themastermind <bertrandmasabo22@gmail.com>
2020-01-23 20:48:55 +0200

    Add user create announcement functionality

---
+++ server/controllers/advertiser.js
@@ -0,0 +1,77 @@
+/* eslint-disable object-shorthand */
+/* eslint-disable no-shadow */
+import moment from 'moment';
+import jwt from 'jsonwebtoken';
+import models from '../models/models';
+import helper from '../helpers/helper';
+import validation from '../middleware/validation';
+import messages from '../utils/messages';
+import utils from '../utils/utils';
+
+// eslint-disable-next-line func-names
+exports.createAnnouncement = (req, res) => {
+  // Joi Validation
+  const { error } = validation.validateCreateAnnouncement(req.body);
+  if (error) {
+    return res.status(400).json({
+      status: 400,
+      error: error.details[0].message,
+    });
+  }
+  // Retrieve token info
+  const token = req.headers.authorization.split(' ')[1];
+  const decoded = jwt.verify(token, process.env.JWT_KEY);
+  req.userData = decoded;
+  // Generate new id
+  const id = helper.getNewId(models.announcements);
+  const data = {
+    title: req.body.title.trim(),
+    description: req.body.description.trim(),
+    startdate: moment(req.body.startdate.trim(), 'MM-DD-YYYY HH:mm').format('x'),
+    enddate: moment(req.body.enddate.trim(), 'MM-DD-YYYY HH:mm').format('x'),
+    status: 'pending',
+    owner: req.userData.id,
+    createdon: moment().format('LLLL'),
+  };
+  // Check if dates are valid
+  const areDatesValid = utils.checkDates(data.startdate, data.enddate);
+  if (!areDatesValid) {
+    return res.status(400).json({
+      status: 400,
+      error: messages.expiredDates,
+    });
+  }
+  // Check if user has permission to create announcements
+  const hasPermission = utils.userCanCreateAnnouncements(data.owner);
+  if (!hasPermission) {
+    return res.status(401).json({
+      status: 401,
+      error: messages.blacklisted,
+    });
+  }
+  // Check if announcement exists already (title and creator)
+  const announcement = utils.announcementExists(data.title, data.owner);
+  if (announcement) {
+    return res.status(400).json({
+      status: 400,
+      error: messages.announcementExists,
+    });
+  }
+  // Save announcement
+  const newAnnouncement = {
+    id,
+    title: data.title,
+    description: data.description,
+    startdate: data.startdate,
+    enddate: data.enddate,
+    status: data.status,
+    owner: data.owner,
+    createdon: data.createdon,
+  };
+  models.announcements.push(newAnnouncement);
+  return res.status(201).json({
+    status: 201,
+    message: messages.announcementCreated,
+    data: newAnnouncement,
+  });
+};
---
+++ server/utils/utils.js
@@ -0,0 +1,41 @@
+import moment from 'moment';
+import models from '../models/models';
+
+// // Check if user exists
+// exports.doesUserExists = (data) => {
+//   const myUser = models.users.find((user) => user.email === data.email);
+//   if (myUser) {
+//     return true;
+//   }
+//   return false;
+// };
+
+// Check if user has permission to create announcements
+exports.userCanCreateAnnouncements = (id) => {
+  const myUser = models.users.find((user) => user.id === id && user.status === 'active');
+  if (myUser) {
+    return true;
+  }
+  return false;
+};
+
+// Check if announcement exists already (title and creator)
+exports.announcementExists = (newTitle, creator) => {
+  const announcement = models.announcements.find(({ title, owner }) => title === newTitle && owner === creator);
+  if (announcement) {
+    return true;
+  }
+  return false;
+};
+
+// Check if startdate and enddate are not in the past
+exports.checkDates = (start, end) => {
+  const now = moment().add(15, 'minutes');
+  if (start < moment(now).format('x') || end < moment(now).format('x')) {
+    return false;
+  }
+  if (start > end) {
+    return false;
+  }
+  return true;
+};
