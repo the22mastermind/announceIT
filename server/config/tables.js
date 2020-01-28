e00910e26146a1d40188542be72c694851b9f6d1
themastermind <bertrandmasabo22@gmail.com>
2020-01-28 13:57:56 +0200

    #170932537 Removed console and promises in tables.js

--- server/config/tables.js
+++ server/config/tables.js
@@ -2,35 +2,28 @@
 import pool from './db';
 
 pool.on('connect', () => {
-  console.log('Connected to PSQL DB');
+  console.log('Connected to DB');
 });
 
 // Drop Tables
 const drop = () => {
-  const users = 'DROP TABLE IF EXISTS users CASCADE;';
-  const announcements = 'DROP TABLE IF EXISTS announcements CASCADE;';
-  const flags = 'DROP TABLE IF EXISTS flags CASCADE;';
-  const con = `${users}; ${announcements}; ${flags};`;
+  const dropUsersTable = 'DROP TABLE IF EXISTS users CASCADE;';
+  const dropAnnouncementsTable = 'DROP TABLE IF EXISTS announcements CASCADE;';
+  const dropFlagsTable = 'DROP TABLE IF EXISTS flags CASCADE;';
+  const dropTables = `${dropUsersTable}; ${dropAnnouncementsTable}; ${dropFlagsTable};`;
 
-  pool.query(con)
-    .then((res) => {
-      console.log(res);
-      pool.end();
-    })
-    .catch((err) => {
-      console.log(err);
-      pool.end();
-    });
+  pool.query(dropTables, () => {
+    pool.end();
+  });
 
   pool.on('remove', () => {
-    console.log('client removed');
     process.exit(0);
   });
 };
 
 // Create Tables
 const create = () => {
-  const queryUsers = `CREATE TABLE IF NOT EXISTS users(
+  const createUsersTable = `CREATE TABLE IF NOT EXISTS users(
       id serial PRIMARY KEY,
       first_name varchar NOT NULL,
       last_name varchar NULL,
@@ -43,7 +36,7 @@ const create = () => {
       createdon timestamptz NOT NULL
       );`;
 
-  const queryAnnouncement = `CREATE TABLE IF NOT EXISTS announcements (
+  const createAnnouncementTable = `CREATE TABLE IF NOT EXISTS announcements (
       id serial PRIMARY KEY,
       picture varchar NULL,
       title varchar NOT NULL,
@@ -55,7 +48,7 @@ const create = () => {
       createdon timestamptz NOT NULL
       );`;
 
-  const queryFlags = `CREATE TABLE IF NOT EXISTS flags (
+  const createFlagsTable = `CREATE TABLE IF NOT EXISTS flags (
       id serial PRIMARY KEY,
       announcement_id int REFERENCES announcements(id) ON DELETE CASCADE,
       reason varchar NOT NULL,
@@ -63,24 +56,17 @@ const create = () => {
       createdon timestamptz NOT NULL
       );`;
 
-  const execute = `
-    ${queryUsers};
-    ${queryAnnouncement};
-    ${queryFlags};
+  const createTables = `
+    ${createUsersTable};
+    ${createAnnouncementTable};
+    ${createFlagsTable};
     `;
 
-  pool.query(execute)
-    .then((res) => {
-      console.log(res);
-      pool.end();
-    })
-    .catch((err) => {
-      console.log(err);
-      pool.end();
-    });
+  pool.query(createTables, () => {
+    pool.end();
+  });
 
   pool.on('remove', () => {
-    console.log('client removed');
     process.exit(0);
   });
 };
